4333402b25399b57def780ae00d3719a4ff685e5
Aayush Vyas <1830002@kiit.ac.in>
2022-03-17 09:39:58 +0530

    MathFunctions added

--- programmaticCalculator/sandbox.js
+++ programmaticCalculator/sandbox.js
@@ -1,45 +1,4 @@
-let firstInput = true;
-const btns = document.querySelectorAll("button");
-let input = "0";
-const display = document.querySelector(".displayInput")
-display.textContent = input;
-
-//clearing the content of the display
-const clear = () => {
-    input = "";
-    display.textContent = input;
-}
-
-//setting the output of the display to 0
-const setZero = () => {
-    input = "0";
-    display.textContent = input;
-}
-//listening for onClick and appending each input to make the targetInput
-btns.forEach(btn => {
-    btn.addEventListener('click',(e) => {
-        //if it is a numerical value then we append it to target
-        btn.style.color = "white";
-        btn.style.background = "black";
-        setTimeout(()=>{
-            btn.style.color = "black";
-            btn.style.background = "white";
-        },1000);
-        if(btn.value >= 1 && btn.value <= 9){
-            if(firstInput == true){
-                firstInput = false;
-                clear();
-            }
-            input+=btn.value;
-            display.textContent = input;
-        }
-        //if the user pressed clear than we set the content to zero
-        else if(btn.value === "clear"){
-            setZero();
-            firstInput = true;
-        }
-    });
-});
+let mathobj;
 class MathFunctions{
     fib = new Map();
     fibonacci(number){
@@ -91,6 +50,102 @@ class MathFunctions{
 
 
 };
+const requiresInput = (operation) => {
+    const singleInput = ["fibonacci", "isPrime", "isPalindrome",
+                            "sort", "isEven","sqrt", "log"];
+    const twoInput = ["GCD", "+", "-", "*", "/", "%", "x^y"];
+    for(let i = 0 ; i <singleInput.length ;i++){
+        if(singleInput[i]  == operation){
+            return 1;
+        }
+    }
+    for(let j = 0 ; j < twoInput.length ;j++){
+        if(twoInput[j]  == operation){
+            return 2;
+        }
+    }
+    
+    return -1;
+};
+
+requiresInput("GCD");
+let firstInput = true;
+const btns = document.querySelectorAll("button");
+let input = "0";
+const display = document.querySelector(".displayInput")
+display.textContent = input;
+//object initializers
+const init = async()  => {
+    let mathobj = new MathFunctions();
+
+}
+init();
+//clearing the content of the display
+const clear = () => {
+    input = "";
+    display.textContent = input;
+}
+
+//setting the output of the display to 0
+const setZero = () => {
+    input = "0";
+    display.textContent = input;
+}
+//control decision
+const decide = (operation)=>{
+    switch (operation){
+        case "Fibonacci" : 
+            takeSecondInput();
+            
+
+    }
+}
+const fetchInput = (Input) => {
+    const fetchedInput = parseInt(Input);
+    return fetchedInput;
+}
+const clearBtn = document.querySelector("#clear");
+const result = document.querySelector("#equals");
+clearBtn.addEventListener('click', setZero());
+
+const takeSecondInput = () => {
+    let secondInputStr = "";
+    const secondInput = document.querySelectorAll("#num");
+    secondInput.forEach( numbtn => {
+        numbtn.addEventListener('click' ,(e) => {
+            secondInputStr+=numbtn.value;
+        })
+    });
+}
+//listening for onClick and appending each input to make the targetInput
+btns.forEach(btn => {
+    btn.addEventListener('click',(e) => {
+        //if it is a numerical value then we append it to target
+        btn.style.color = "white";
+        btn.style.background = "black";
+        setTimeout(()=>{
+            btn.style.color = "black";
+            btn.style.background = "white";
+        },1000);
+        if(btn.value >= 1 && btn.value <= 9){
+            if(firstInput == true){
+                firstInput = false;
+                clear();
+            }
+            input+=btn.value;
+            display.textContent = input;
+        }
+        //if the user pressed clear than we set the content to zero
+        else if(btn.value === "clear"){
+            setZero();
+            firstInput = true;
+        }
+        else{
+            decide(btn.value);
+        }
+    });
+});
+
 
 
 
